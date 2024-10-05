d6388ef41dc271d77b529d428cbe821ada06988f
WarrenWu4 <warrenweiwu04@gmail.com>
2024-10-05 13:52:46 -0500

    hungry monkey

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+export default function App() {
+
+  return (
+    <>
+        <nav className="navbar">
+            <div className="logo">
+
+                <a href="/">aicuity</a>
+            </div>
+            <div className="links">
+                <a href="/">home</a>
+                <a href="/about">about</a>
+            </div>
+        </nav>
+        <div>
+            <div className="initial-content"></div>
+            <div className="main-content">
+                
+            </div>
+        </div>
+        <footer className="footer">
+            made with &lt;3 by the Aicuity team
+        </footer>
+    </>
+  )
+}
