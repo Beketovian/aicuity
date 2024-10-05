83042606775b06b4abe57d272a159a43feab7eb5
xProioxis <christionb@hotmail.com>
2024-10-05 14:13:03 -0500

    chaned format of transcript retrieval

--- backend/yt_transcript.py
+++ backend/yt_transcript.py
@@ -1,5 +1,7 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
+def get_yt_transcript(video_id):
+    transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
+    return transcript
 
-video_id = "vHx2YwT3GrY"
-transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
+
