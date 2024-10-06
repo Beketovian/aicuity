5aa8a75f98e8cb0ddea6fd5b30030a99367f0d6a
xProioxis <christionb@hotmail.com>
2024-10-06 02:29:27 -0500

    added video highlights

--- backend/pipeline.py
+++ backend/pipeline.py
@@ -2,20 +2,22 @@ import text_sim
 import yt_transcript
 import yt_vid_list
 
-query = "How to find the inverse of a mod"
+query = "How to do Long Division"
 vids = yt_vid_list.get_top_videos(query)
 
 texts_list = []
 
 for vid in vids:
     transcript = yt_transcript.get_yt_transcript(vid)
-    full_text = [elem["text"] for elem in transcript]
-    full_text = " ".join(full_text)
-    texts_list.append(full_text)
+    texts_list.append(transcript)
 
-scores = text_sim.do_text_sim(query, texts_list)
+scores, relevant_times = text_sim.do_text_sim(query, texts_list)
 
 max_score = max(scores)
 max_idx = scores.index(max_score)
 
-print("Similarity Score:", max_score, "https://www.youtube.com/watch?v=" + vids[max_idx])
+# min_score = min(scores)
+# min_idx = scores.index(min_score)
+
+# print("Min Similarity Score:", min_idx, "https://www.youtube.com/watch?v=" + vids[min_idx])
+print("Max Similarity Score Index:", max_idx, "https://www.youtube.com/watch?v=" + vids[max_idx], relevant_times[max_idx])
--- backend/text_sim.py
+++ backend/text_sim.py
@@ -8,28 +8,48 @@ tokenizer_model = None
 def cosine_similarity(a, b):
     return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
-common_words = {"the":"","in":"","that":"","can":"","of":"","this":"","can":"","be":"","into":"",
-"is":"","a":"","to":"","of":"","and":""}
+def softmax(x):
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
 
-def do_text_sim(query, corpus):
+common_words = {"the":"","in":"","that":"","can":"","of":"","this":"","can":"","be":"","into":"",
+"is":"","a":"","to":"","of":"","and":"", "how":"", "do":""}
+
+def do_text_sim(query, input_dict):
     global tokenizer_model
 
     if tokenizer_model is None:
         tokenizer_model = api.load('word2vec-google-news-300')
 
+    corpus = []
+    for elems in input_dict:
+        full_text = [txt["text"] for txt in elems]
+        full_text = " ".join(full_text)
+        corpus.append(full_text)
+    
     search_term = query.split()
+    search_term = [word.lower() for word in search_term]
     search_embed = [tokenizer_model[word.lower()] for word in search_term if word.lower() in tokenizer_model]
     test = [word for word in search_term if word.lower() in tokenizer_model]; print(test)
     avg_search_embed = np.mean(search_embed, axis=0)
 
     scores = []
-    for k in range(len(corpus)):
-        terms = corpus[k].split()
-        word_embeddings = [tokenizer_model[word.lower()] for word in terms if word.lower() in tokenizer_model and word.lower() not in common_words]
+    relevant_times = {i:[] for i in range(len(input_dict))}
+    
+    for i in range(len(input_dict)):
+        word_embeddings = []
+        for j in range(len(input_dict[i])):
+            terms = input_dict[i][j]["text"].split()
+            #word_embeddings = [tokenizer_model[word.lower()] for word in terms if word.lower() in tokenizer_model and word.lower() not in common_words]
+            for word in terms:
+                if word.lower() in tokenizer_model and word.lower() not in common_words:
+                    word_embeddings.append(tokenizer_model[word.lower()])
+                if word.lower() in search_term and word.lower() not in common_words:
+                    relevant_times[i].append(input_dict[i][j]["start"])
+                
         if len(word_embeddings) == 0:
             scores.append(0); continue
         avg_embed = np.mean(word_embeddings, axis=0)
         scores.append(cosine_similarity(avg_search_embed, avg_embed))
     
-    return scores
+    return softmax(scores).tolist(), relevant_times
 
